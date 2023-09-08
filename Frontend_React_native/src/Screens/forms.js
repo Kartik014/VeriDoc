71b15e2f9027cd7585fa4d4f70c7abeb042c71ba
Kartik014 <99882037+Kartik014@users.noreply.github.com>
2023-09-08 22:24:06 +0530

    IMPLEMENTED FORMS

--- Frontend_React_native/src/Screens/forms.js
+++ Frontend_React_native/src/Screens/forms.js
@@ -1,13 +1,14 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import { ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
 import SelectDropdown from 'react-native-select-dropdown'
 import styles from '../Styles';
 
-const Forms = () => {
+const Forms = ({ navigation }) => {
 
     const [exerciseNo, setExerciseNo] = useState('')
     const [day, setDay] = useState('')
     const [exerciseInput, setExerciseInput] = useState([])
+    const [diet, setDiet] = useState({ breakfast: '', lunch: '', dinner: '', snacks: '' })
     const [setsNo, setSetsNo] = useState('')
     const days = ["Monday", "Tuesday", "Wednesday", "Thrusday", "Friday", "Saturday", "Sunday"]
 
@@ -17,7 +18,7 @@ const Forms = () => {
             inputs.push(
                 <TextInput
                     key={i}
-                    style={styles1.elementText}
+                    style={[styles.forms.elementText, { flex: 1 }]}
                     placeholder={`Exercise ${i + 1}`}
                     placeholderTextColor={'gray'}
                     value={exerciseInput[i] || ''}
@@ -33,89 +34,101 @@ const Forms = () => {
         setExerciseInput(updateInput)
     }
 
-    // const consoleIt = () => {
-    //     for(let i=0; i<exerciseNo; i++ ){
-    //         console.log(`${exerciseInput[i]}`)
-    //     }
-    // }
+    const handleDietChange = (key, value) => {
+        setDiet(prevDiet => ({
+            ...prevDiet,
+            [key]: value,
+        }))
+    }
 
     const exerciseInputs = generateExerciseInput(exerciseNo)
 
     return (
-        <View style={styles1.formContainer}>
-            <View style={styles1.element}>
-                <Text style={styles1.elementText}>SELECT DAY: </Text>
-                <SelectDropdown
-                    data={days}
-                    onSelect={(selectedItem) => {
-                        setDay(selectedItem);
-                    }}
-                    buttonTextAfterSelection={(selectedItem) => {
-                        return selectedItem;
-                    }}
-                    rowTextForSelection={(item) => {
-                        return item;
-                    }}
-                />
-            </View>
-            <View style={styles1.element}>
-                <Text style={styles1.elementText}>TOTAL NUMBER OF EXERCISE: </Text>
-                <TextInput
-                    style={{ color: 'black' }}
-                    placeholder='TEXT'
-                    value={exerciseNo}
-                    onChangeText={value => {
-                        setExerciseNo(value)
-                        setExerciseInput(Array.from({ length: parseInt(value, 10) }, () => ''))
-                    }}
-                    keyboardType='numeric'
-                />
-            </View>
-            {exerciseInputs.map((input, index) => (
-                <View key={index} style={styles1.element}>
-                    {input}
+        <ScrollView contentContainerStyle={styles.forms.scrollContainer}>
+            <View style={styles.forms.formContainer}>
+                <View style={styles.forms.element}>
+                    <Text style={styles.forms.elementText}>SELECT DAY: </Text>
+                    <SelectDropdown
+                        data={days}
+                        onSelect={(selectedItem) => {
+                            setDay(selectedItem);
+                        }}
+                        buttonTextAfterSelection={(selectedItem) => {
+                            return selectedItem;
+                        }}
+                        rowTextForSelection={(item) => {
+                            return item;
+                        }}
+                    />
                 </View>
-            ))}
-            {/* <View>
-                <TouchableOpacity
-                style= {styles.signup}
-                onPress={()=>consoleIt()}>
-                    <Text>consoleIt</Text>
-                </TouchableOpacity>
-            </View> */}
-            <View style={styles1.element}>
-                <Text style={styles1.elementText}>TOTAL NUMBER OF SETS: </Text>
-                <TextInput
-                    style={{ color: 'black' }}
-                    placeholder='TEXT'
-                    value={setsNo}
-                    onChangeText={value => setSetsNo(value)}
-                    keyboardType='numeric'
-                />
+                <View style={styles.forms.element}>
+                    <Text style={styles.forms.elementText}>TOTAL NUMBER OF EXERCISE: </Text>
+                    <TextInput
+                        style={{ color: 'black' }}
+                        placeholder='TEXT'
+                        value={exerciseNo}
+                        onChangeText={value => {
+                            setExerciseNo(value)
+                            setExerciseInput(Array.from({ length: parseInt(value, 10) }, () => ''))
+                        }}
+                        keyboardType='numeric'
+                    />
+                </View>
+                {exerciseInputs.map((input, index) => (
+                    <View key={index} style={styles.forms.element}>
+                        {input}
+                    </View>
+                ))}
+                <View style={styles.forms.element}>
+                    <Text style={styles.forms.elementText}>TOTAL NUMBER OF SETS: </Text>
+                    <TextInput
+                        style={{ color: 'black' }}
+                        placeholder='TEXT'
+                        value={setsNo}
+                        onChangeText={value => setSetsNo(value)}
+                        keyboardType='numeric'
+                    />
+                </View>
+                <View style={[styles.forms.element, { flexDirection: 'column' }, { alignItems: 'stretch' }]}>
+                    <Text style={styles.forms.elementText}>DIET PLAN</Text>
+                    <View style={styles.forms.element}>
+                        <Text style={styles.forms.elementText}>BREAKFAST: </Text>
+                        <TextInput
+                            style={[{ color: 'black' }, { flex: 1 }]}
+                            value={diet.breakfast}
+                            onChangeText={value => handleDietChange('breakfast', value)} />
+                    </View>
+                    <View style={styles.forms.element}>
+                        <Text style={styles.forms.elementText}>LUNCH: </Text>
+                        <TextInput
+                            style={[{ color: 'black' }, { flex: 1 }]}
+                            value={diet.lunch}
+                            onChangeText={value => handleDietChange('lunch', value)} />
+                    </View>
+                    <View style={styles.forms.element}>
+                        <Text style={styles.forms.elementText}>DINNER: </Text>
+                        <TextInput
+                            style={[{ color: 'black' }, { flex: 1 }]}
+                            value={diet.dinner}
+                            onChangeText={value => handleDietChange('dinner', value)} />
+                    </View>
+                    <View style={styles.forms.element}>
+                        <Text style={styles.forms.elementText}>SNACKS: </Text>
+                        <TextInput
+                            style={[{ color: 'black' }, { flex: 1 }]}
+                            value={diet.snacks}
+                            onChangeText={value => handleDietChange('snacks', value)} />
+                    </View>
+                </View>
+                <View style={{ alignItems: 'center', margin: 10 }}>
+                    <TouchableOpacity style={styles.forms.elementButton}
+                        onPress={() => navigation.navigate('Home')}>
+                        <Text style={styles.forms.elementText}>SAVE</Text>
+                    </TouchableOpacity>
+                </View>
             </View>
-        </View>
+        </ScrollView>
     );
 };
-
-const styles1 = StyleSheet.create({
-    formContainer: {
-        flex: 1,
-        padding: 20,
-    },
-    element: {
-        borderRadius: 5,
-        padding: 10,
-        backgroundColor: 'white',
-        elevation: 10,
-        shadowColor: 'black',
-        alignItems: 'center',
-        flexDirection: 'row',
-        margin: 10
-    },
-    elementText: {
-        margin: 5,
-        color: 'black',
-    }
-})
 
 export default Forms;
