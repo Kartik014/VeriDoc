098015fabc216d32dfd2e5427677bc40a9f62fbf
Kartik014 <99882037+Kartik014@users.noreply.github.com>
2023-09-13 17:37:41 +0530

    ADDING FUNCTIONALITY TO HOME SCREEN

--- Frontend_React_native/src/Screens/home.js
+++ Frontend_React_native/src/Screens/home.js
@@ -1,15 +1,57 @@
-import React from "react";
-import { Text, View, TouchableOpacity } from "react-native";
+import React, { useEffect, useState } from "react";
+import { Text, View, TouchableOpacity, FlatList, ScrollView } from "react-native";
+import firestore from '@react-native-firebase/firestore';
+import auth from '@react-native-firebase/auth';
+import styles from "../Styles";
 
-const Home = ({navigation}) =>{
+const Home = ({ navigation }) => {
+
+    const currentUser = auth().currentUser
+    const [dayNames, setDayNames] = useState([])
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const userDoc = await firestore().collection("training_program").doc("training_programs").get()
+                const userData = userDoc.data()
+
+                console.log(userData)
+
+                const Data = userData[currentUser.uid]
+                if (Data && Data.exercise) {
+                    const dayNameArray = Object.keys(Data.exercise)
+                    setDayNames(dayNameArray)
+                }
+
+            } catch (err) {
+                console.error(err)
+            }
+        }
+
+        fetchData()
+    }, [])
+
     return (
-        <View>
-            <Text>HOME SCREEN</Text>
-            <TouchableOpacity
-            onPress={()=>navigation.navigate('Forms')}>
-                <Text>ADD BUTTON</Text>
-            </TouchableOpacity>
-        </View>
+        <ScrollView contentContainerStyle={styles.forms.scrollContainer}>
+            <View style={styles.forms.formContainer}>
+                <FlatList
+                    data={dayNames}
+                    keyExtractor={(item, index) => index.toString()}
+                    renderItem={({ item }) => (
+                        <TouchableOpacity style={[styles.forms.element, { justifyContent: 'center' }]} onPress={() => console.log("pressed ", { item })}>
+                            <Text style={[styles.forms.elementText]}>{item}</Text>
+                        </TouchableOpacity>
+                    )}
+                />
+                <View style={{ alignItems: 'center', margin: 10 }}>
+                    <TouchableOpacity
+                        style={styles.forms.elementButton}
+                        onPress={() => navigation.navigate('Forms')}>
+                        <Text style={styles.forms.elementText}>ADD BUTTON</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </ScrollView >
     )
 }
 
